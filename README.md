c7406b6af345ad0f07ee117cd02bf30bb6c40d9f
mlimper <max.limper@googlemail.com>
2023-12-29 14:08:38 +0100

    First working version

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
-# blockreplace
+# BlockReplace (BloRP)
 
-Replaces placeholders in documents (navbar, ...) with HTML "components", for cases where really simple and efficient websites are desired without using any JS frameworks (client-side assembly) or other complex build steps (with bloated dependency trees etc).
+Replaces placeholders in documents (navbar, ...) with HTML "components", for cases where really simple, yet efficient websites are desired (e.g., personal homepage)
 
-WARNING: If you're a modern web developer, this project might hurt your personal feelings. Investigate and use at your own risk.
+## Goals and Scope
+This tool allows you to build a simple website that has *navbar and footer or similar re-occurring components* defined *exactly once*, *without* using any
+
+* JS frameworks that perform client-side composition of your page,
+* complex PHP bakers that would do the same server-side or
+* other complex build steps with potentially bloated dependency trees etc.
+
+In contrast, this is just a simple python script without any dependencies, which replaces stuff in your HTML so you can build a simple, static homepage with pure HTML and CSS, and that's really all it is or does.
+
+## Sample Use
+```
+python .\blorp.py -i input-sample -o output-sample
+```
+
+## Limitations
+* In contrast to ShadowDOM or related ideas, this script really has the sole objective of making it possible to have the re-occuring bits of a website (usually: navbar and footer) being defined only once. At the end, everything will just be part of the same website, and share the same namespace in CSS, etc. (which might not be a problem for many simple projects, but potentially for bigger apps, and this little script here really doesn't claim to solve that all).
+* Indentation is read from the input block file and kept as-is, block definitions should not have any indentation in the input HTML file.
+* Block definitions must currently be 100% matching the schema - including (absence of) whitespace and lower/upper case, etc.
+* Currently, the input directory and output directory will be considered flat (subdirectory structures will not be parsed or replicated).
+
+
+WARNING: If you're a modern web developer, this project might hurt your personal honor or feelings. Investigate and use at your own risk! ;-)
+
+## LICENSE
+This tool is available under the MIT license.
---
+++ blorp.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+
+from argparse import ArgumentParser
+from pathlib import Path
+
+
+# #############################################################################
+# argument parsing
+# #############################################################################
+
+parser = ArgumentParser()
+
+parser.add_argument("-i", "--inputDirectory",  help="input directory",  default=".")
+parser.add_argument("-o", "--outputDirectory", help="output directory", default="out")
+
+pArgs    = parser.parse_args()
+argsDict = vars(pArgs)
+
+inputDirectory  = argsDict["inputDirectory"]
+outputDirectory = argsDict["outputDirectory"]
+
+
+# #############################################################################
+# global variables and utility functions
+# #############################################################################
+
+BlockFileExt  = ".block"
+BlockTagStart = "<blorp>"
+BlockTagEnd   = "</blorp>"
+
+def getBlockStr(blockName, blockFiles):
+    for bf in blockFiles:
+        if bf.stem == blockName:
+            with open(bf) as blockFileHandle:
+                return blockFileHandle.read()
+   
+    errorMsg = "ERROR: Could not replace block \"" + blockName + "\""
+    print(errorMsg)    
+    return "<strong>" + errorMsg + "</strong>"
+
+
+# #############################################################################
+# main script
+# #############################################################################
+
+htmlFiles  = Path(inputDirectory).glob('*.html')
+blockFiles = Path(inputDirectory).glob('*' + BlockFileExt)
+
+for htmlFile in htmlFiles:
+
+    with open(htmlFile) as htmlFileHandle:
+
+        htmlFileContent    = htmlFileHandle.read()
+        replacedBlockNames = []
+        newHTMLFileContent = htmlFileContent
+        
+        blockIdx = htmlFileContent.find(BlockTagStart)        
+
+        # parse blocks within the file and replace them
+        while blockIdx != -1:
+            blockEndIdx = htmlFileContent.find(BlockTagEnd, blockIdx + len(BlockTagStart))            
+
+            # very basic error check
+            if (blockEndIdx == -1):
+                print("ERROR: Incomplete block. Must start with \"" + BlockTagStart +
+                      "\" and end with \"" + BlockTagEnd + "\".")
+                blockIdx = htmlFileContent.find(BlockTagStart, blockIdx + 1)
+                continue
+
+            # parse block name        
+            blockName = htmlFileContent[blockIdx+len(BlockTagStart):blockEndIdx]
+
+            if blockName in replacedBlockNames:
+                blockIdx = htmlFileContent.find(BlockTagStart, blockIdx + 1)
+                continue
+
+            fullBlockNew = getBlockStr(blockName, blockFiles)
+
+            # perform replacement for all instances
+            fullBlockOld = htmlFileContent[blockIdx:blockEndIdx + len(BlockTagEnd)]
+            newHTMLFileContent = newHTMLFileContent.replace(fullBlockOld, fullBlockNew)
+
+            # remember that this block name has been processed for this HTML files
+            replacedBlockNames.append(blockName)            
+         
+            blockIdx = htmlFileContent.find(BlockTagStart, blockIdx + 1)
+
+        # write output file        
+        outputFilePath = Path(outputDirectory).joinpath(htmlFile.name)
+
+        with open(outputFilePath, "w") as htmlOutputFileHandle:
+            htmlOutputFileHandle.write(newHTMLFileContent)
